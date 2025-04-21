e5b5447dbc90934d8e5706a4e4a5b988ea17d814
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 01:36:51 +0500

    feat logging_service: add kafka konfig values

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"os"
+	"strconv"
 )
 
 type (
 	Config struct {
-		DbCfg DbConfig
+		DbCfg       *DbConfig
+		ConsumerCfg *ConsumerConfig
 	}
 
 	DbConfig struct {
@@ -16,16 +18,35 @@ type (
 		Password string
 		Database string
 	}
+
+	ConsumerConfig struct {
+		Brokers          string
+		GroupID          string
+		Topic            string
+		AutoOffsetReset  string
+		EnableAutoCommit bool
+		MaxPoolInterval  int
+		SessionTimeOut   int
+	}
 )
 
 func New() *Config {
 	return &Config{
-		DbCfg: DbConfig{
+		DbCfg: &DbConfig{
 			Host:     getEnv("DB_HOST", "localhost"),
 			Port:     getEnv("DB_PORT", "9000"),
 			User:     getEnv("DB_USER", "default"),
 			Password: getEnv("DB_PASSWORD", ""),
 			Database: getEnv("DB_DATABASE", "default"),
+		},
+		ConsumerCfg: &ConsumerConfig{
+			Brokers:          getEnv("BROKERS", "localhost:9092"),
+			GroupID:          getEnv("GROUP_ID", "logging_consumer"),
+			Topic:            getEnv("KAFKA_TOPIC", "logs"),
+			AutoOffsetReset:  getEnv("AUTO_OFFSET_RESET", "earliest"),
+			EnableAutoCommit: getEnvBool("ENABLE_AUTO_COMMIT", false),
+			MaxPoolInterval:  getEnvInt("MAX_POOL_INTERVAL", 300000),
+			SessionTimeOut:   getEnvInt("SESSION_TIME_OUT", 30000),
 		},
 	}
 }
@@ -34,6 +55,24 @@ func New() *Config {
 func getEnv(key, fallback string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
+	}
+	return fallback
+}
+
+func getEnvBool(key string, fallback bool) bool {
+	if value := os.Getenv(key); value != "" {
+		return value == "true"
+	}
+	return fallback
+}
+
+func getEnvInt(key string, fallback int) int {
+	if value := os.Getenv(key); value != "" {
+		v, err := strconv.Atoi(value)
+		if err != nil {
+			return fallback
+		}
+		return v
 	}
 	return fallback
 }
