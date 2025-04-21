417988632e78ed237f90248927730fbefa277ba5
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 00:23:25 +0500

    feat logging_service: implement migrations for click-house

---
+++ migrations/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS logs (
+	id UUID DEFAULT generateUUIDv4(),
+	message String,
+	event_time DateTime,
+	received_at DateTime DEFAULT now()
+) ENGINE = MergeTree
+PARTITION BY toYYYYMM(event_time)
+ORDER BY (event_time)
+TTL event_time + INTERVAL 30 DAY
+SETTINGS index_granularity = 8192;
