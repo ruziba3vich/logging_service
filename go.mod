0a23a8d2d8af16cf586c2f304dacecea2948a57a
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 01:41:57 +0500

    feat logging_service: implement consumer

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.2
 
 require (
 	github.com/ClickHouse/clickhouse-go/v2 v2.34.0
+	github.com/confluentinc/confluent-kafka-go v1.9.2
 	google.golang.org/grpc v1.72.0
 	google.golang.org/protobuf v1.36.6
 )
@@ -15,10 +16,8 @@ require (
 	github.com/go-faster/errors v0.7.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
@@ -27,6 +26,5 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -1,1 +1,100 @@
 package consumer
+
+import (
+	"context"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/ruziba3vich/logging_service/genprotos/genprotos/logging_service"
+	"github.com/ruziba3vich/logging_service/internal/service"
+	"github.com/ruziba3vich/logging_service/pkg/config"
+	"google.golang.org/protobuf/proto"
+)
+
+type LogConsumer struct {
+	consumer       *kafka.Consumer
+	loggingService *service.LoggingService
+	topic          string
+	isRunning      bool
+}
+
+func NewLogConsumer(
+	cfg *config.Config,
+	loggingService *service.LoggingService,
+) (*LogConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":    cfg.ConsumerCfg.Brokers,
+		"group.id":             cfg.ConsumerCfg.GroupID,
+		"auto.offset.reset":    cfg.ConsumerCfg.AutoOffsetReset,
+		"enable.auto.commit":   cfg.ConsumerCfg.EnableAutoCommit,
+		"max.poll.interval.ms": cfg.ConsumerCfg.MaxPoolInterval,
+		"session.timeout.ms":   cfg.ConsumerCfg.SessionTimeOut,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &LogConsumer{
+		consumer:       c,
+		loggingService: loggingService,
+		topic:          cfg.ConsumerCfg.Topic,
+		isRunning:      false,
+	}, nil
+}
+
+func (lc *LogConsumer) Start(ctx context.Context) error {
+	if lc.isRunning {
+		return nil
+	}
+
+	err := lc.consumer.Subscribe(lc.topic, nil)
+	if err != nil {
+		return err
+	}
+
+	lc.isRunning = true
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				lc.Stop()
+				return
+			default:
+				msg, err := lc.consumer.ReadMessage(-1)
+				if err != nil {
+					log.Printf("Error reading message: %v", err)
+					continue
+				}
+
+				pbLog := &logging_service.Log{}
+				if err := proto.Unmarshal(msg.Value, pbLog); err != nil {
+					log.Printf("Error unmarshaling protobuf message: %v", err)
+					lc.consumer.CommitMessage(msg)
+					continue
+				}
+
+				_, err = lc.loggingService.SendLog(ctx, pbLog)
+				if err != nil {
+					log.Printf("Failed to process log: %v", err)
+					continue
+				}
+
+				_, err = lc.consumer.CommitMessage(msg)
+				if err != nil {
+					log.Printf("Failed to commit offset: %v", err)
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (lc *LogConsumer) Stop() {
+	if !lc.isRunning {
+		return
+	}
+	lc.isRunning = false
+	lc.consumer.Close()
+}
