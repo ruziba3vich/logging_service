0126655c01948737b6580e4a2cc79f8c13d31f0d
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 00:31:47 +0500

    feat logging_service: implement storage

--- internal/storage/logging_storage.go
+++ internal/storage/logging_storage.go
@@ -1,5 +1,32 @@
 package storage
 
+import (
+	"context"
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/ruziba3vich/logging_service/genprotos/genprotos/logging_service"
+)
+
 type LoggingStorage struct {
-	
+	db *sql.DB
+}
+
+func NewLoggingStorage(db *sql.DB) *LoggingStorage {
+	return &LoggingStorage{db: db}
+}
+
+func (s *LoggingStorage) StoreLog(ctx context.Context, l *logging_service.Log) {
+	query := `INSERT INTO logs (message, event_time) VALUES (?, ?)`
+
+	_, err := s.db.ExecContext(ctx, query, l.Message, l.EventTime.AsTime())
+	if err != nil {
+		log.Printf("Failed to store log: %v. Trying to log this error.", err)
+		errorMsg := "Failed to store log: " + err.Error()
+		_, innerErr := s.db.ExecContext(ctx, query, errorMsg, time.Now())
+		if innerErr != nil {
+			log.Printf("Failed to store internal error log too: %v", innerErr)
+		}
+	}
 }
