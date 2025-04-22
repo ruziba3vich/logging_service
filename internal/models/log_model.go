3f1ef1e936021ffe63001b162b9045aaf99fb499
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 16:53:18 +0500

    fix logging_service: fix model field

--- internal/models/log_model.go
+++ internal/models/log_model.go
@@ -5,7 +5,7 @@ import "time"
 type Log struct {
 	ID         string `gorm:"type:UUID;default:generateUUIDv4()"`
 	Message    string
-	EventTime  time.Time
+	EventTime  string
 	Level      string
 	Service    string
 	ReceivedAt time.Time `gorm:"autoCreateTime"`
