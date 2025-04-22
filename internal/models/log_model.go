8c185b59394226222f842b7985e920b31836b119
ruziba3vich <rruzibayevich@gmail.com>
2025-04-22 16:58:15 +0500

    fix logging_service: fix model field

--- internal/models/log_model.go
+++ internal/models/log_model.go
@@ -5,7 +5,7 @@ import "time"
 type Log struct {
 	ID         string `gorm:"type:UUID;default:generateUUIDv4()"`
 	Message    string
-	EventTime  string
+	EventTime  time.Time
 	Level      string
 	Service    string
 	ReceivedAt time.Time `gorm:"autoCreateTime"`
--- internal/storage/logging_storage.go
+++ internal/storage/logging_storage.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/ruziba3vich/logging_service/genprotos/genprotos/logging_service"
+	"github.com/ruziba3vich/logging_service/internal/models"
 	"gorm.io/gorm"
 )
 
@@ -18,14 +19,14 @@ func NewLoggingStorage(db *gorm.DB, insertQuery string) *LoggingStorage {
 
 // query := `INSERT INTO logs (message, event_time, level, service) VALUES (?, ?, ?, ?)`
 func (s *LoggingStorage) StoreLog(ctx context.Context, l *logging_service.Log) error {
-	// log := Log{
-	// 	Message:   l.Message,
-	// 	EventTime: l.EventTime.AsTime().Format("2006-01-02 15:04:05"), // format for ClickHouse
-	// 	Level:     l.Level,
-	// 	Service:   l.Service,
-	// }
-	// if err := s.db.WithContext(ctx).Create(&log).Error; err != nil {
-	// 	return err
-	// }
+	log := models.Log{
+		Message:   l.Message,
+		EventTime: l.EventTime.AsTime().Format("2006-01-02 15:04:05"), // format for ClickHouse
+		Level:     l.Level,
+		Service:   l.Service,
+	}
+	if err := s.db.WithContext(ctx).Create(&log).Error; err != nil {
+		return err
+	}
 	return nil
 }
